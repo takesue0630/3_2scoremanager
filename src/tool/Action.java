1d29aad841922dcdca434db4fe1e2ce5542dfa98
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-04-24 10:29:58 +0900

    インターフェースからクラスへの変更

--- src/tool/Action.java
+++ src/tool/Action.java
@@ -1,11 +1,10 @@
 package tool;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public interface Action {
-    void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
+public abstract class Action {
+	public abstract String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception;
 }
