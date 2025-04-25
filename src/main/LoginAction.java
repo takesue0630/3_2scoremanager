669d568032505607866cae35f6de083e3fcce7f0
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-04-25 10:18:40 +0900

    旧ログインアクションの追加

---
+++ src/main/LoginAction.java
@@ -0,0 +1,32 @@
+package main;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import dao.TeacherDao;
+import tool.Action;
+
+public class LoginAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+
+		HttpSession session=request.getSession();
+
+		String id=request.getParameter("id");
+		String password=request.getParameter("password");
+
+		TeacherDao dao=new TeacherDao();
+		Teacher teacher=dao.login(id, password);
+
+		if (teacher!=null) {
+
+			session.setAttribute("teacher", teacher);
+			return "menu.jsp";
+		}
+
+		return "login.jsp";
+	}
+}
