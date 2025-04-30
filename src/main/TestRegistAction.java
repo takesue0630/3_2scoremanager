888d389cb614a4525ccb0dc985efe658a8dd40bf
takumi <kmm2459322@stu.o-hara.ac.jp>
2025-04-30 10:12:40 +0900

    TestRegistAction.javaの追記

--- src/main/TestRegistAction.java
+++ src/main/TestRegistAction.java
@@ -2,14 +2,28 @@ package main;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.Teacher;
+import dao.ClassNumDao;
+import dao.SubjectDao;
 import tool.Action;
 
 public class TestRegistAction extends Action{
 	public String execute(
 			HttpServletRequest request, HttpServletResponse response
 		) throws Exception {
-		System.out.println("s");
+		HttpSession session=request.getSession();
+		Teacher teacher=(Teacher)session.getAttribute("teacher");
+
+		//学校のクラスデータを取得
+		ClassNumDao classdao = new ClassNumDao();
+		classdao.filter(teacher.getSchool());
+
+		//学校の科目データを取得
+		SubjectDao subjectdao = new SubjectDao();
+		subjectdao.filter(teacher.getSchool());
+
 		return "test_regist.jsp";
 	}
 }
