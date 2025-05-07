6edce27d161ee5ba5aa10ad7e706038348defc7e
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-05-07 10:33:45 +0900

    新規作成

---
+++ src/main/TestListStudentExecuteAction.java
@@ -0,0 +1,53 @@
+package main;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import bean.Teacher;
+import tool.Action;
+
+public class TestListStudentExecuteAction extends Action {
+	public String execute(
+		HttpServletRequest request, HttpServletResponse response
+	) throws Exception {
+		//セッションからユーザーデータを取得
+		HttpSession session=request.getSession();
+		Teacher teacher=(Teacher)session.getAttribute("teacher");
+
+//		検索項目の取得
+//		入学年度
+		int ent_year=-1;
+		if (request.getParameter("f1")!=null) {
+			ent_year=Integer.parseInt(request.getParameter("f1"));
+		}
+//		クラス番号
+		String class_num=request.getParameter("f2");
+//		科目
+		String subject=request.getParameter("f3");
+
+//		入学年度、クラス、科目のいずれかが未入力の場合
+		if (ent_year==-1 || class_num==null || subject==null) {
+//			エラーのセット
+			request.setAttribute("error", "入学年度とクラスと科目を選択してください");
+//			成績一覧画面へ戻す
+			return "test_regist.jsp";
+		}
+
+//		入学年度、クラス、科目に一致する成績データを取得
+		TestListSubjectDao test_list_subject_dao=new TestListSubjectDao();
+		List<TestListSubject> list=test_list_subject_dao.filter(ent_year, class_num, subject, teacher.getSchool());
+//		リクエスト属性に成績のリストを格納
+		request.setAttribute("list", list);
+		request.setAttribute("size", list.size());
+
+//		入力欄用データ
+		request.setAttribute("ent_year", ent_year);
+		request.setAttribute("class_num", class_num);
+		request.setAttribute("subject", subject);
+
+		return "test_list_subject.jsp";
+	}
+}
