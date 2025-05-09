0fa77fa3783aa4c86835bd26dcd481d597f9cecf
takumi <kmm2459322@stu.o-hara.ac.jp>
2025-05-09 09:36:00 +0900

    Action.javaを戻した

--- src/tool/Action.java
+++ src/tool/Action.java
@@ -1,30 +1,15 @@
 package tool;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public abstract class Action extends HttpServlet { // HttpServlet を継承
-    private static final long serialVersionUID = 1L;
+public abstract class Action {
+    public abstract String execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception;
 
-    public abstract String execute(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException;
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String path = execute(request, response);
-        if (path != null) {
-            request.getRequestDispatcher(path).forward(request, response);
-        }
-    }
-
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        doPost(request, response);
-    }
+	public String execute(HttpServletRequest request) throws Exception {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
 }
