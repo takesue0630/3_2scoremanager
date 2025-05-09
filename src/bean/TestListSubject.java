2de8ec77a4a92788a550f3d7d882e4561e22a3a6
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-05-09 10:41:11 +0900

    内容の記述

--- src/bean/TestListSubject.java
+++ src/bean/TestListSubject.java
@@ -1,5 +1,61 @@
 package bean;
 
-public class TestListSubject {
+import java.io.Serializable;
+import java.util.Map;
 
+public class TestListSubject implements Serializable {
+
+    private int entYear;
+    private String studentNo;
+    private String studentName;
+    private String classNum;
+    private Map<Integer,Integer> points;
+
+    public int getEntYear() {
+        return entYear;
+    }
+
+    public void setEntYear(int entYear) {
+        this.entYear = entYear;
+    }
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public void setSubjectcd(String studentNo) {
+        this.studentNo = studentNo;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+
+    public Map<Integer,Integer> getPoints() {
+    	return points;
+    }
+
+    public void setPoints(Map<Integer,Integer> points) {
+    	this.points = points;
+    }
+
+    public String getPoint(int key) {
+    	return String.valueOf(points.get(key));
+    }
+
+    public void setPoint(int key,int value) {
+    	this.points.put(key, value);
+    }
 }
