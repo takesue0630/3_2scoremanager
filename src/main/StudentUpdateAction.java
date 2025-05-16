e05f13b51c0d5247438a86aea51a3ff57254879f
takesue0630 <kmm2459389@stu.o-hara.ac.jp>
2025-05-16 10:29:02 +0900

    エラー遷移の追加

--- src/main/StudentUpdateAction.java
+++ src/main/StudentUpdateAction.java
@@ -16,23 +16,28 @@ public class StudentUpdateAction extends Action {
 	public String execute(
 		HttpServletRequest request, HttpServletResponse response
 	) throws Exception {
-//		セッションの取得
-		HttpSession session=request.getSession();
+		try {
+//			セッションの取得
+			HttpSession session=request.getSession();
 
-//		受け取った学生番号から変更する学生の情報を持ってくる
-		StudentDao s=new StudentDao();
-		Student student=s.get(request.getParameter("no"));
+//			受け取った学生番号から変更する学生の情報を持ってくる
+			StudentDao s=new StudentDao();
+			Student student=s.get(request.getParameter("no"));
 
-		Teacher teacher=(Teacher)session.getAttribute("teacher");
-		ClassNumDao dao=new ClassNumDao();
-		List<String> class_num=dao.filter(teacher.getSchool());
+			Teacher teacher=(Teacher)session.getAttribute("teacher");
+			ClassNumDao dao=new ClassNumDao();
+			List<String> class_num=dao.filter(teacher.getSchool());
 
-		request.setAttribute("ent_year", student.getEntYear());
-		request.setAttribute("no", student.getNo());
-		request.setAttribute("name", student.getName());
-		request.setAttribute("is_attend", student.getIsAttend());
-		request.setAttribute("class_num", class_num);
+			request.setAttribute("ent_year", student.getEntYear());
+			request.setAttribute("no", student.getNo());
+			request.setAttribute("name", student.getName());
+			request.setAttribute("is_attend", student.getIsAttend());
+			request.setAttribute("class_num", class_num);
 
-		return "student_update.jsp";
+			return "student_update.jsp";
+
+		} catch(Exception e) {
+			return "../error.jsp";
+		}
 	}
 }
